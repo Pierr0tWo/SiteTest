ba9bd535b2d5d3e94b8024aabc12f431ac13014e
Pierr0tWo <144243949+Pierr0tWo@users.noreply.github.com>
2023-12-15 12:28:49 +0000

    gg

--- Site/SiteJS/SiteJS3/script.js
+++ Site/SiteJS/SiteJS3/script.js
@@ -1,3 +1,9 @@
-function maFonction() {
-	alert("Le JavaScript fonctionne !")
+let ratio = 0.8 ;
+
+function setup() {
+  createCanvas(windowWidth*ratio, windowHeight*ratio);
+}
+
+function draw() {
+  ellipse(mouseX, mouseY, 80, 80);
 }
